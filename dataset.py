dfbd16bc555da71a6d01d00b115e8aa6d65e62d2
Emma Kristovich <emkristovich@gmail.com>
2019-11-17 08:58:09 -0800

    rectangular radius, definitely a real thing

--- dataset.py
+++ dataset.py
@@ -27,7 +27,7 @@ class DataSet():
         if remove_outliers:
             self.remove_outliers()
 
-    # Returns a cleaned dataframe with the columns of the features removed  
+    # Returns a cleaned dataframe with the columns of the features removed
     # features: an array of strings of feature names
     # ex. ['mpg', 'car name']
     # df (optional): dataframe, if not specified will use self.df
@@ -75,12 +75,57 @@ class DataSet():
 
     # Returns a df of sites within a specified radius proportion
     def get_df_of_radius(self, proportion, df=None):
-        longitude_center, latitude_center = get_center_coordinates()
-        
+        # Notes on proportion: this is not the proportion of sites to keep
+        # Proportion is (proportion of area to keep around center)^2
+        #   That means a proportion of 0.5 keeps a quarter of the total area
+
+        # The main problem with this method is you lose the min & max for both
+        # latitude and longitude until the proportion is exactly 1
+
+        # proportion        % remaining
+        # 0.1               8.8
+        # 0.25              13.1
+        # 0.5               44.8
+        # 0.75              71.4
+        # 0.9               78.6
+        # 0.95 - 0.9999     89.2
+
+        # center of mass of points
+        long_center, lat_center = self.get_center_coordinates()
+
+        # min and max of latitude and longitude
+        long_min, long_max = self.get_min_max("long")
+        lat_min, lat_max = self.get_min_max("lat")
+
+        # acceptable range is a box that grows with proportion away from the
+        # center of mass
+        long_range = [long_center - (long_center - long_min) * proportion,
+                        long_center + (long_max - long_center) * proportion]
+        lat_range = [lat_center - (lat_center - lat_min) * proportion,
+                        lat_center + (lat_max - lat_center) * proportion]
+
+        # sites within the acceptable range
+        valid_sites = []
+        for site in SITEDICT.keys():
+            if (float(SITEDICT[site]["lat"]) >= lat_range[0] and
+                float(SITEDICT[site]["lat"]) <= lat_range[1] and
+                float(SITEDICT[site]["long"]) >= long_range[0] and
+                float(SITEDICT[site]["long"]) <= long_range[1]):
+                valid_sites.append(site)
+
+        if df is None:
+            df = self.df
+
+        return df[df["Site Id"].isin(valid_sites)]
+
 
     def get_center_coordinates(self):
         sites = list(SITEDICT.keys())
         longitude_center = mean(float(SITEDICT[site]["long"]) for site in sites)
         latitude_center = mean(float(SITEDICT[site]["lat"]) for site in sites)
         return longitude_center, latitude_center
-            
+
+    def get_min_max(self, key_str):
+        minv = min(float(SITEDICT[site][key_str]) for site in SITEDICT.keys())
+        maxv = max(float(SITEDICT[site][key_str]) for site in SITEDICT.keys())
+        return minv, maxv
--- main.py
+++ main.py
@@ -8,8 +8,12 @@ from sitedict import *
 
 def main():
     dataset = DataSet()
-    print(dataset.get_colnames())
+    # print(dataset.get_colnames())
     # print (SITEDICT)
+    before = len(dataset.df.index)
+    new_df = dataset.get_df_of_radius(1)
+    after = len(new_df.index)
+    print(f"Proportion remaining: {after/before}")
 
 
 if __name__ == "__main__":
